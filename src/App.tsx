45f4ff534b81e9906d843abdb25d0bd2a083c89a
Boyan Bogdanov <boyan.bogdanov@accedia.com>
2022-10-10 00:18:06 +0300

    rework root component to render 1 chart depending on what is chosen with a select

--- src/App.tsx
+++ src/App.tsx
@@ -1,100 +1,106 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+import { SyntheticEvent, useState } from "react";
 import DonutChart from "./DonutChart";
 
-const FAKE_DATA_ONE = [
-  {
-    id: 1,
-    percent: 35,
-  },
-  {
-    id: 2,
-    percent: 10,
-  },
-  {
-    id: 3,
-    percent: 10,
-  },
-  {
-    id: 4,
-    percent: 10,
-  },
-  {
-    id: 5,
-    percent: 10,
-  },
-  {
-    id: 6,
-    percent: 25,
-  },
+const MOCK_DATA = [
+  [
+    {
+      id: 1,
+      percent: 35,
+    },
+    {
+      id: 2,
+      percent: 10,
+    },
+    {
+      id: 3,
+      percent: 10,
+    },
+    {
+      id: 4,
+      percent: 10,
+    },
+    {
+      id: 5,
+      percent: 10,
+    },
+    {
+      id: 6,
+      percent: 25,
+    },
+  ],
+  [
+    {
+      id: 1,
+      percent: 10,
+    },
+    {
+      id: 2,
+      percent: 20,
+    },
+    {
+      id: 3,
+      percent: 15,
+    },
+    {
+      id: 4,
+      percent: 30,
+    },
+    {
+      id: 5,
+      percent: 10,
+    },
+    {
+      id: 6,
+      percent: 15,
+    },
+  ],
+  [
+    {
+      id: 1,
+      percent: 30,
+    },
+    {
+      id: 2,
+      percent: 30,
+    },
+    {
+      id: 3,
+      percent: 40,
+    },
+  ],
 ];
 
-const FAKE_DATA_TWO = [
-  {
-    id: 1,
-    percent: 10,
-  },
-  {
-    id: 2,
-    percent: 20,
-  },
-  {
-    id: 3,
-    percent: 15,
-  },
-  {
-    id: 4,
-    percent: 30,
-  },
-  {
-    id: 5,
-    percent: 10,
-  },
-  {
-    id: 6,
-    percent: 15,
-  },
-];
+const App = () => {
+  const [selectedDataIndex, setSelectedDataIndex] = useState(0);
 
-const FAKE_DATA_THREE = [
-  {
-    id: 1,
-    percent: 30,
-  },
-  {
-    id: 2,
-    percent: 30,
-  },
-  {
-    id: 3,
-    percent: 40,
-  },
-];
+  const handleSelectChange = (event: { target: { value: any } }) => {
+    setSelectedDataIndex(event.target.value);
+  };
 
-function App() {
   return (
-    <div style={{ display: "flex", flexDirection: "row", gap: 125 }}>
-      <DonutChart
-        viewBox={100}
-        radius={40}
-        borderSize={17.5}
-        data={FAKE_DATA_ONE}
-      />
-
-      <DonutChart
-        viewBox={100}
-        radius={40}
-        borderSize={17.5}
-        data={FAKE_DATA_TWO}
-      />
-
-      <DonutChart
-        viewBox={100}
-        radius={40}
-        borderSize={17.5}
-        data={FAKE_DATA_THREE}
-      />
-    </div>
+    <>
+      <select
+        value={selectedDataIndex}
+        style={{ marginBottom: "50px" }}
+        onChange={handleSelectChange}
+      >
+        {MOCK_DATA.map((data, index) => (
+          <option key={index} value={index}>
+            {data.map((d) => String(d.percent)).reduce((a, b) => a + " " + b)}
+          </option>
+        ))}
+      </select>
+      <div>
+        <DonutChart
+          viewBox={100}
+          radius={40}
+          borderSize={17.5}
+          data={MOCK_DATA[selectedDataIndex]}
+        />
+      </div>
+    </>
   );
-}
+};
 
 export default App;
